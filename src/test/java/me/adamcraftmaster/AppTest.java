51783513b210f750f938499aa14d7be6d7b7dfc2
Adam Nativ <admlvntv@gmail.com>
2021-11-05 19:36:35 -0400

    Unit Tests
    checks that the checkValid() function works with a simple test

--- src/test/java/me/adamcraftmaster/AppTest.java
+++ src/test/java/me/adamcraftmaster/AppTest.java
@@ -1,20 +1,28 @@
 package me.adamcraftmaster;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * Unit test for checking if the checkValid function works with both functional and non functional values.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void checkValidTest() {
+        App tester = new App();
+        //tester.debug = 1;
+        for(int x = 1; x <= 8; x++) {
+            //System.out.println("Testing " + x);
+            if(x==2) {
+                //System.out.println(tester.checkValid(2,1, x,8));
+                assertFalse(tester.checkValid(2,1, x,8));
+            }
+            else {
+                //System.out.println(tester.checkValid(2,1, x,8));
+                assertTrue(tester.checkValid(2,1, x,8));
+            }
+        }
     }
 }
